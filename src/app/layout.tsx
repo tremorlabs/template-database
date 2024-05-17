d832ba2dd8cbb8daddcdf3654a54d8518e1b8c85
severinlandolt <sev.landolt@gmail.com>
2024-05-17 20:41:31 +0200

    update banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ function Banner() {
     <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition hover:-translate-y-px">
       <a
         href="https://blocks.tremor.so/templates"
-        className="rounded-full bg-gray-950 px-4 py-2 shadow-lg shadow-black/20 ring-1 ring-white/10 hover:bg-gray-900"
+        className="rounded-full bg-gray-950 px-6 py-3 shadow-lg shadow-black/20 ring-1 ring-white/10 hover:bg-gray-900"
       >
         <span className="font-medium text-white">
           Get this template for $79
