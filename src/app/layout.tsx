8c85f532c1e61175b7423d3a94a988b4cc63bd16
severinlandolt <sev.landolt@gmail.com>
2024-07-20 01:40:26 +0200

    lint and remove banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { ArrowAnimated } from "@/components/ui/ArrowAnimated"
 import Footer from "@/components/ui/Footer"
 import { Navigation } from "@/components/ui/Navbar"
 import type { Metadata } from "next"
@@ -44,23 +43,6 @@ export const metadata: Metadata = {
   },
 }
 
-function Banner() {
-  return (
-    <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition">
-      <div className="flex items-center gap-x-1 rounded-full bg-gray-950 p-1 text-sm shadow-xl shadow-black/20 ring-1 ring-white/10">
-        <a
-          className="group flex items-center gap-0.5 whitespace-nowrap rounded-[20px] bg-gradient-to-b from-white to-gray-200 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-indigo-400/30 transition"
-          href="https://blocks.tremor.so/templates#template-database"
-          target="_blank"
-        >
-          Get this template
-          <ArrowAnimated />
-        </a>
-      </div>
-    </div>
-  )
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -75,7 +57,6 @@ export default function RootLayout({
           <Navigation />
           {children}
           <Footer />
-          <Banner />
         </ThemeProvider>
       </body>
     </html>
